cc73962c43fea34242ea7191815cfbd2b469c5fb
sanmgin9412 <tkdals9048@naver.com>
2021-11-29 20:22:31 +0900

    update: config.js path 수정

--- config/config.js
+++ config/config.js
@@ -1,1 +1,2 @@
-export const path = process.env.NODE_ENV === 'production' ? 'https://sangmin9412.github.io/xr-global' : '';
+// export const path = process.env.NODE_ENV === 'production' ? 'https://sangmin9412.github.io/xr-global' : '';
+export const path = process.env.NODE_ENV === 'production' ? '/xr-global' : '';
